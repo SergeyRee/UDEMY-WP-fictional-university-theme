0c804c1e7261757e00ead256e7847802b787a02a
SergeyRee <drrons2000@gmail.com>
2021-03-26 11:17:33 +0200

    User Roles and Permissions

--- functions.php
+++ functions.php
@@ -1,117 +1,151 @@
-<?php 
+<?php
 
 require get_theme_file_path('/inc/search-route.php');
 
 function university_custom_rest() {
-    register_rest_field('post', 'authorName', array(
-        'get_allback' => 'function () { return get_the_author(); }'
-    ));
-};
+  register_rest_field('post', 'authorName', array(
+    'get_callback' => function() {return get_the_author();}
+  ));
+}
 
 add_action('rest_api_init', 'university_custom_rest');
 
 function pageBanner($args = NULL) {
-    
-    if (!$args['title']) {
-        $args['title'] = get_the_title();
-    }
+  
+  if (!$args['title']) {
+    $args['title'] = get_the_title();
+  }
 
-    if (!$args['subtitle']) {
-        $args['subtitle'] = get_field('page_banner_subtitle');
-    }
+  if (!$args['subtitle']) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
 
-    if (!$args['photo']) {
-        if (get_field('page_banner_background_image') AND !is_archive() AND !is_home()) {
-            $args['photo'] = get_field('page_banner_background_image') ['sizes'] ['pageBanner'];
-        } else {
-            $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
-        }
+  if (!$args['photo']) {
+    if (get_field('page_banner_background_image')) {
+      $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
     }
+  }
 
-    ?>
-    <div class="page-banner">
-            <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
-            <div class="page-banner__content container container--narrow">
-                <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
-                <div class="page-banner__intro">
-                    <p><?php echo $args['subtitle']; ?></p>
-                </div>
-            </div>
-        </div>  
+  ?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>  
+  </div>
 <?php }
 
 function university_files() {
-    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDin3iGCdZ7RPomFLyb2yqFERhs55dmfTI', NULL, '1.0', true);
 
-    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=apiKeyApiKeyApiKeyApiKey', NULL, '1.0', true);   
+  if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
+    wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+  } else {
+    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.6acc14197e9cd6afafcc.js'), NULL, '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.35fe72f820fc43850c20.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.35fe72f820fc43850c20.css'));
+  }
 
-    if (strstr($_SERVER['SERVER_NAME'], 'localhost')) {
-        wp_enqueue_script('main_university_js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);   
-    } else {
-        wp_enqueue_script('our_vendors_js', get_theme_file_uri('/bundled-assets/vendors~scripts.cac342a964e6d29b701a.js'), NULL, '1.0', true);   
-        wp_enqueue_script('main_university_js', get_theme_file_uri('/bundled-assets/scripts.b76d7c41404527e57837.js'), NULL, '1.0', true);
-        wp_enqueue_style('our_main_styles', get_theme_file_uri('/bundled-assets/styles.b76d7c41404527e57837.css'));
-    }
-
-    wp_localize_script('main_university_js', 'universityData', array(
-        'root_url' => get_site_url(),
-        
-    ));
-    
+  wp_localize_script('main-university-js', 'universityData', array(
+    'root_url' => get_site_url()
+  ));
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features() {
-    // register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    // register_nav_menu('footerLocationOne', 'Footer Location One');
-    // register_nav_menu('footerLocationTwo', 'Footer Location Two');
-
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
-    add_image_size('professorLandscape', 400, 260, true);
-    add_image_size('professorPortret', 480, 650, true);
-    add_image_size('pageBanner', 1500, 350, true);
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape', 400, 260, true);
+  add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query) {
-    $today = date('Ymd');
-    
-    if (!is_admin() AND is_post_type_archive('campus') AND is_main_query()) {    
-        $query->set('posts_per_page', -1);
-    }
-    
-    if (!is_admin() AND is_post_type_archive('program') AND is_main_query()) {
-        $query->set('orderby', 'title');
-        $query->set('order', 'ASC');
-        $query->set('posts_per_page', -1);
-    }
+  if (!is_admin() AND is_post_type_archive('campus') AND is_main_query()) {
+    $query->set('posts_per_page', -1);
+  }
 
-    if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
-        //$query->set('posts_per_page', '1');
-        $query->set('meta_key', 'event_date');
-        $query->set('orderby', 'meta_value_num');
-        $query->set('order', 'ASC');
-        $query->set('meta_query', array(
-            array(
+  if (!is_admin() AND is_post_type_archive('program') AND is_main_query()) {
+    $query->set('orderby', 'title');
+    $query->set('order', 'ASC');
+    $query->set('posts_per_page', -1);
+  }
+
+  if (!is_admin() AND is_post_type_archive('event') AND is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+              array(
                 'key' => 'event_date',
                 'compare' => '>=',
                 'value' => $today,
-                'type' => 'numeric'     
-            )
-        ));
-    }
+                'type' => 'numeric'
+              )
+            ));
+  }
 }
 
-add_action( 'pre_get_posts', 'university_adjust_queries');
+add_action('pre_get_posts', 'university_adjust_queries');
 
-// function universityMapKey($api) {
-//     $api['key'] = 'apiKeyApiKeyApiKeyApiKey';
-//     return $api;
-// }
+function universityMapKey($api) {
+  $api['key'] = 'AIzaSyBh9b1rNCp6kOi5JeMHiRP4klDymBeoEWk';
+  return $api;
+}
 
-// add_filter('acf/fields/google_map/api', 'universityMapKey');
-?>
+add_filter('acf/fields/google_map/api', 'universityMapKey');
+
+
+// Redirect subscriber accounts out of admin and onto homepage
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend() {
+  $ourCurrentUser = wp_get_current_user();
+
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    wp_redirect(site_url('/'));
+    exit;
+  }
+}
+
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar() {
+  $ourCurrentUser = wp_get_current_user();
+
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+
+// Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+function ourHeaderUrl() {
+  return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginCSS() {
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.35fe72f820fc43850c20.css'));
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle() {
+  return get_bloginfo('name');
+}
